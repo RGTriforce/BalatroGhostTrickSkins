d3c1acedb4ffbc31253e22b59c2a431a41285efd
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-12-22 23:42:31 -0300

    Format lua files with StyLua

--- collabs/spades.lua
+++ collabs/spades.lua
@@ -1,39 +1,39 @@
 --- Spades
 --- Atlas
-SMODS.Atlas {
-    key = "spadesLow",
-    path = "spades_low.png",
-    px = 71,
-    py = 95
-};
+SMODS.Atlas({
+	key = "spadesLow",
+	path = "spades_low.png",
+	px = 71,
+	py = 95,
+})
 
-SMODS.Atlas {
-    key = "spadesHigh",
-    path = "spades_high.png",
-    px = 71,
-    py = 95
-};
+SMODS.Atlas({
+	key = "spadesHigh",
+	path = "spades_high.png",
+	px = 71,
+	py = 95,
+})
 
 --- Pathing
-SMODS.DeckSkin {
-    key = "Ghost Trick Spades",
-    suit = "Spades",
-    loc_txt = "Ghost Trick",
-    palettes = {
-        {
-            key = "lc",
-            ranks = {"Jack", "Queen", "King"},
-            display_ranks = {"King", "Queen", "Jack"},
-            atlas = "gtskin_spadesLow",
-            pos_style = "ranks",
-        },
-        {
-            key = "hc",
-            ranks = {"Jack", "Queen", "King"},
-            display_ranks = {"King", "Queen", "Jack"},
-            atlas = "gtskin_spadesHigh",
-            pos_style = "ranks",
-            hc_default = true,
-        }
-    },
-}
+SMODS.DeckSkin({
+	key = "Ghost Trick Spades",
+	suit = "Spades",
+	loc_txt = "Ghost Trick",
+	palettes = {
+		{
+			key = "lc",
+			ranks = { "Jack", "Queen", "King" },
+			display_ranks = { "King", "Queen", "Jack" },
+			atlas = "gtskin_spadesLow",
+			pos_style = "ranks",
+		},
+		{
+			key = "hc",
+			ranks = { "Jack", "Queen", "King" },
+			display_ranks = { "King", "Queen", "Jack" },
+			atlas = "gtskin_spadesHigh",
+			pos_style = "ranks",
+			hc_default = true,
+		},
+	},
+})
