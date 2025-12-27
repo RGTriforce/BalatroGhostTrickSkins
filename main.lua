13276ca4a50365231eac837d648beec43617d8c8
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-12-26 21:47:00 -0300

    Add modicon

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+SMODS.Atlas({
+	key = "modicon",
+	path = "modicon.png",
+	px = 34,
+	py = 34,
+})
+
 assert(SMODS.load_file("collabs/spades.lua"))()
 assert(SMODS.load_file("collabs/hearts.lua"))()
 assert(SMODS.load_file("collabs/clubs.lua"))()
